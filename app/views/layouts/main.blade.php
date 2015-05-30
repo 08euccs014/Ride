8b5edb8fa46e23f8090156066505de3dcda9fca5
Mohit Agrawal <er.magrawal@gmail.com>
2015-05-30 08:50:14 +0530

    Improvement :: meta tag and keyword added for seo.

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -7,7 +7,14 @@
 <!doctype html>
 <html lang="en">
 <head>
+
+	<base href="http://www.joinmyway.net" />
 	<meta charset="UTF-8">
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="best ride pool, best ride share, car pool, ride pool, pooling, motorcycle pool, 2 wheeler pool, 4 wheeler pool, want a ride, save fuel, save moeny, join my way, joinmyway, pick me up, ride share, rideshare, share ride" />
+	<meta name="description" content="Join my way is ride pool initiative to help those people who does not have their own vehicle but travel a lot on same way. so it will bring the rider and passenger togather to join their to save money for both rider and passenger. it will save fuel, save money, save environment from pollution. it help reducing commuting time and traffic." />
+
+
 	<title>Join My Way : Best Ride Pooling Initiative</title>
 	{{ HTML::style('assets/css/bootstrap.min.css') }}
 	{{ HTML::style('assets/css/override.css') }}
