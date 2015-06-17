425e2d5d9c6fb152ed43724c00176fadc79cc7bf
Mohit Agrawal <er.magrawal@gmail.com>
2015-06-17 05:49:20 +0530

    Fix :: now riders will be listed in recently registered first.

--- app/models/rider.php
+++ app/models/rider.php
@@ -68,10 +68,10 @@ class riderModel extends Eloquent implements UserInterface, RemindableInterface 
 				}
 			}
 
-			$riders = $this->select('*')->whereIn('id', $riderIds)->where('verify', '=', 1)->paginate(RIDER_PAGINATION);
+			$riders = $this->select('*')->whereIn('id', $riderIds)->where('verify', '=', 1)->orderBy('created_at', 'desc')->paginate(RIDER_PAGINATION);
 		}
 	    else {
-			$riders = $this->select('*')->where('id', '!=', $loggedIn)->where('verify', '=', 1)->paginate(RIDER_PAGINATION);
+			$riders = $this->select('*')->where('id', '!=', $loggedIn)->where('verify', '=', 1)->orderBy('created_at', 'desc')->paginate(RIDER_PAGINATION);
 		}
     	}
     	catch (Exception $e) {
