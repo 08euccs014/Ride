46057d3b210b7ec7270d516026cf3ecf97a04ff6
Mohit Agrawal <er.magrawal@gmail.com>
2015-06-17 06:20:58 +0530

    Improvement :: footer updated.

--- app/views/layouts/footer.blade.php
+++ app/views/layouts/footer.blade.php
@@ -15,16 +15,25 @@
 <!-- 	</div> -->
 
 <!-- 	<hr/> -->
-	<div class="col-md-12 gap-bottom-20">
+	<div class="col-md-12 gap-bottom-20 black-link">
 		<div class="col-md-4 text-center">
 			<p><a href="{{ url('privacy_policies') }}"  target="_blank">Privacy Policies</a></p>
 			<p><a href="{{ url('term_and_conditions') }}"  target="_blank">Terms & Conditions</a></p>
 		</div>
 		<div class="col-md-4 text-center">
-			<a href="{{ url('aboutus') }}" target="_blank">Know About Us</a>
+			<div class="row text-center">
+			<a  href="{{ url('aboutus') }}" target="_blank">
+				<img src="{{ url('assets/image/msn.svg') }}" height="50px" width="50px"/>
+				<p class="gap-top-10"> Know About Us</p>
+			</a>
+			</div>
 		</div>
 		<div class="col-md-4 text-center">
-			&copy;All copyright reserved @joinmyway.net 
+			<div class="row">
+				<a class="col-md-3 col-md-offset-3" href="http://facebook.com/joinmyway.net" target="_blank"><img src="{{ url('assets/image/facebook.svg') }}" height="50px" width="50px"/></a>
+				<a class="col-md-3" href="https://twitter.com/joinmyway" target="_blank"><img src="{{ url('assets/image/twitter.svg') }}" height="50px" width="50px"/></a>
+			</div>
+			<div class="gap-top-10">&copy;All copyright reserved @joinmyway.net </div>
 		</div>
 	</div>
 @stop
